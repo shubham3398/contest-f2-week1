18f866217d46072e71d1378ae40bf4bb0b63ffc4
vshubham331998 <vshubham331998@gmail.com>
2023-11-18 15:08:00 +0530

    contest submission

--- script.js
+++ script.js
@@ -4,31 +4,96 @@ const data = [
 ];
 
 // 1. Print Developers
-function printDeveloper() {}
+function printDeveloper() {
+  for(let i = 0; i < data.length; i++){
+    if (data[i].profession === "developer") console.log(data[i]);
+  }
+}
 
 // 2. Add Data
-function addData() {}
+function addData() {
+  let a = prompt("Enter Your Name");
+  let b = prompt("Enter Your Age");
+  let c = prompt("Enter Your Profession");
+
+  data.push(
+    { name : a , age : parseInt(b), profession : c}
+  );
+}
 
 // 3. Remove Admins
-function removeAdmin() {}
+function removeAdmin() {
+  for(let i in data){
+    if(data[i].profession === "admin") {
+      data.splice(i, 1);
+    }
+  }
+}
 
 // 4. Concatenate Array
-function concatenateArray() {}
+function concatenateArray() {
+  let arr1 = [1, 2, 3, 4];
+  let arr2 = [5, 6, 7, 8];
+
+  let arr = arr1.concat(arr2);
+  console.log(arr);
+}
 
 // 5. Average Age
-function averageAge() {}
+function averageAge() {
+  let ageSum = 0;
+  for(let iterate of data){
+    ageSum += iterate.age;
+  }
+  let averageAge = (ageSum / data.length);
+
+  console.log(averageAge);
+}
 
 // 6. Age Check
-function checkAgeAbove25() {}
+function checkAgeAbove25() {
+  for(let iterate of data){
+    if(iterate.age > 25) {
+      console.log("age above 25 is present");
+      return true;
+    }
+  }
+  return false;
+}
 
 // 7. Unique Professions
-function uniqueProfessions() {}
+function uniqueProfessions() {
+  let unique = new Map();
+
+  for(let i of data){
+    if(unique.has(i.profession) === false){
+      unique.set(i.profession, i.name);
+      console.log(i);
+    }
+  }
+}
 
 // 8. Sort by Age
-function sortByAge() {}
+function sortByAge() {
+  data.sort((a, b) => a.age - b.age);
+}
 
 // 9. Update Profession
-function updateJohnsProfession() {}
+function updateJohnsProfession() {
+  for(let i of data){
+    if(i.name === "john"){
+      i.profession = 'manager';
+    }
+  }
+}
 
 // 10. Profession Count
-function getTotalProfessions() {}
+function getTotalProfessions() {
+  let count = 0;
+  for(let i of data){
+    if(i.profession === 'developer' || i.profession === 'admin'){
+      count++;
+    }
+  }
+  console.log(count);
+}
